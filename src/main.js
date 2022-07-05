4f12663b48c32015647644c427e613a3ecf54348
Neel Singh <neel@rocketbots.io>
2022-07-05 18:08:07 +0800

    PoC for i18next

--- src/main.js
+++ src/main.js
@@ -4,10 +4,43 @@ import App from '@/App.vue'
 import store from '@/store'
 import router from '@/router'
 
+// localization packages
+import i18next from "i18next";
+import I18NextVue from "i18next-vue";
+import HttpApi from 'i18next-http-backend';
+
+
 Vue.config.productionTip = false
 
-new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount("#app")
+Vue.use(I18NextVue, { i18next });
+
+// on i18n init
+i18next.on("initialized", () => {
+    new Vue({
+        router,
+        store,
+        render: h => h(App)
+    }).$mount("#app")
+});
+
+// can be used to change dependencies locale
+i18next.on('languageChanged', function(lng) {})
+
+// can be used to warn if a key is missing
+i18next.on('missingKey', function(lngs, namespace, key, res) {
+    console.error({lngs, key});
+})
+
+
+i18next
+  .use(HttpApi)
+  .init({
+      lng: 'en',
+      fallbackLng: 'en',
+      preload: ['en'],
+      debug: true,
+      backend: {
+          // for all available options read the backend's repository readme file
+          loadPath: '/locales/{{lng}}.json',
+      }
+  })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,41 @@
 <template>
-  <div>THIS IS HOME</div>
+  <div>
+    <h1>Interpolation: {{ $t('welcome') }}</h1>
+    <h1>Computed: {{ welcome }}</h1>
+    <h1>With filter: {{ $t('welcome') | lowercase }}</h1>
+    <select @change="onlanguagechange">
+      <option value="en">en</option>
+      <option value="es">es</option>
+    </select>
+  </div>
+
 </template>
+
 <script>
 import HomeApi from "@/api/modules/HomeApi";
 export default {
   name: "Home",
   components: {},
-  computed: {},
-  data: () => ({}),
-  beforeCreate() {},
-  created() {},
-  beforeMount() {},
-  mounted() {},
-  beforeUpdate() {},
-  updated() {},
-  activated() {}, //when keep-alive
-  deactivated() {}, //when keep-alive
-  beforeDestroy() {},
-  destroyed() {},
+  computed: {
+    welcome() {
+      return this.$t('welcome')
+    }
+  },
+  filters: {
+    lowercase(value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.toLowerCase();
+    }
+  },
   methods: {
     // getDatList(){
     //   HomeApi.getDatList()
     // }
+    async onlanguagechange({target}) {
+      // await this.$i18next.loadLanguages([target.value]);
+      await this.$i18next.changeLanguage(target.value)
+    }
   },
 };
 </script>
