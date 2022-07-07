764e63906c9ce73ee00000635d1eb3dfeb66262d
Neel Singh <neel@rocketbots.io>
2022-07-07 16:08:42 +0800

    - Add i18next-browser-languagedetector - Simulate user preferred language

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import router from '@/router'
 import i18next from "i18next";
 import I18NextVue from "i18next-vue";
 import HttpApi from 'i18next-http-backend';
-
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 Vue.config.productionTip = false
 
@@ -31,16 +31,30 @@ i18next.on('missingKey', function(lngs, namespace, key, res) {
     console.error({lngs, key});
 })
 
+// simulate fetch user preference
+setTimeout(() => {
+  i18next.changeLanguage('en')
+}, 3000)
+
 
 i18next
+  .use(LanguageDetector)
   .use(HttpApi)
   .init({
-      lng: 'en',
       fallbackLng: 'en',
       preload: ['en'],
       debug: true,
       backend: {
           // for all available options read the backend's repository readme file
           loadPath: '/locales/{{lng}}.json',
+      },
+      detection: {
+        // order and from where user language should be detected
+        order: ['preference', 'querystring', 'localStorage'],
+        // keys or params to lookup language from
+        lookupQuerystring: 'lang',
+        // cache user language on
+        caches: ['localStorage'],
+        excludeCacheFor: ['cimode'], // languages to not persist (cookie, localStorage)
       }
   })
